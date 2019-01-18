9a91c68dd5a7a8978779c86b9c7183d58da08870
octoccoper <octocopper@gmail.com>
2019-01-18 20:10:12 +0200

    updated app.js and storage.js

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+// Initialize Storage object
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
+
 // Initialize weather object
-const weather = new Weather("London");
+const weather = new Weather(weatherLocation.city);
 
 // Initialize ui object
 const ui = new UI();
 
+
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Modal window for change location. Event listener for the button. 
+document.getElementById("w-change-btn").addEventListener("click", (e) => { 
+  const city = document.getElementById("city").value;
+
+  // Change location in ui
+  weather.changeLocation(city);
+
+  // Save location to LocalStorage
+  storage.setLocationData(city);
+
+  // Display the weather
+  getWeather();
+
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather.getWeather()
---
+++ storage.js
@@ -0,0 +1,21 @@
+class Storage { 
+  constructor() { 
+    this.city;
+    this.defaultCity = "London";
+  }
+
+  getLocationData() {
+    if ((localStorage.getItem("city") === null) && (localStorage.getItem("city") === undefined)) {
+      this.city = this.defaultCity;
+    } else { 
+      this.city = localStorage.getItem("city");
+    }
+    return {
+      city: this.city
+    }
+   }
+
+  setLocationData() { 
+    localStorage.setItem("city", city);
+  }
+}
